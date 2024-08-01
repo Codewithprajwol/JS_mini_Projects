bfe97253ed9c855a67658611197d871206da8279
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2024-08-01 11:16:13 +0545

    not completed but more precised and reusable code is implemented

--- String Transfromation/script.js
+++ String Transfromation/script.js
@@ -1,33 +1,31 @@
 const mainInput=document.querySelector('#input');
-const Input=document.querySelector('#input1');
-const Input1=document.querySelector('#input2')
-const Input2=document.querySelector('#input3')
-const Input3=document.querySelector('#input4')
-const Input4=document.querySelector('#input5')
-const Input5=document.querySelector('#input6')
+const upperCase=document.querySelector('#input1')
+const lowerCase=document.querySelector('#input2');
+const cameCase=document.querySelector('#input3')
+const snakeCase=document.querySelector('#input4')
+const KebabCase=document.querySelector('#input5')
+const pascalCase=document.querySelector('#input6')
+const trim=document.querySelector('#input5')
 
-mainInput.addEventListener('input',(e)=>{
 
 
-    const Data=mainInput.value;
-    Input.value=Data.toUpperCase();
-    Input1.value=Data.toLowerCase();
-    Input4.value=Data.replaceAll(' ','_');
-    Input3.value=Data.replaceAll(' ','-');
-})
 
-// const testData='my name is Prajwol Shrestha'
 
-// function Camel(Data){
-//    finalData=[...Data]
-// finalData[0].toUpperCase();
-//     finalData.forEach((data,index)=>{
-//         if(data.includes(' ')){
-//        finalData[index+1].toUpperCase()
-//        console.log(finalData)
-//         }
-//      })
-
+// function textTransform({data=mainInput.value.trim()}){
+//     // const camelCase=data.toLowerCase().split(' ').reduce((a,b)=>a + b[0]?.toUpperCase()+b.substring(1).toLowerCase());
+// lowerCase.textContent=data.toLowerCase();
 // }
 
-// Camel(testData);
+// mainInput.addEventListener('input',textTransform);
+// textTransform({})
+
+function textTransform({ data } = {}) {
+    data = data || mainInput.value.trim();
+
+    lowerCase.textContent = data.toLowerCase();
+}
+
+mainInput.addEventListener('input', () => textTransform({}));
+
+
+
