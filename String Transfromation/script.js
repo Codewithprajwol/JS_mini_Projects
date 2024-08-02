61855a6290eba7d5bcefc4069f08f85873b7fa85
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2024-08-02 19:53:35 +0545

    string transformation min-project is completed with responsiveness

--- String Transfromation/script.js
+++ String Transfromation/script.js
@@ -1,15 +1,11 @@
 const mainInput=document.querySelector('#input');
 const upperCase=document.querySelector('#input1')
 const lowerCase=document.querySelector('#input2');
-const cameCase=document.querySelector('#input3')
-const snakeCase=document.querySelector('#input4')
-const KebabCase=document.querySelector('#input5')
-const pascalCase=document.querySelector('#input6')
-const trim=document.querySelector('#input5')
-
-
-
-
+const camelCase=document.querySelector('#input3')
+const KebabCase=document.querySelector('#input4')
+const snakeCase=document.querySelector('#input5')
+const trim=document.querySelector('#input6')
+const pascalCase=document.querySelector('#input7')
 
 // function textTransform({data=mainInput.value.trim()}){
 //     // const camelCase=data.toLowerCase().split(' ').reduce((a,b)=>a + b[0]?.toUpperCase()+b.substring(1).toLowerCase());
@@ -18,14 +14,17 @@ const trim=document.querySelector('#input5')
 
 // mainInput.addEventListener('input',textTransform);
 // textTransform({})
-
 function textTransform({ data } = {}) {
     data = data || mainInput.value.trim();
-
+const camelCaseOutput=data.toLowerCase().split(' ').reduce((a,b)=>a + b[0]?.toUpperCase()+b.substring(1).toLowerCase());
     lowerCase.textContent = data.toLowerCase();
+    upperCase.textContent= data.toUpperCase();
+    camelCase.textContent=camelCaseOutput;
+    pascalCase.textContent=camelCaseOutput ? camelCaseOutput[0].toUpperCase()+camelCaseOutput.substring(1) : '';
+    trim.textContent=data;
+    snakeCase.textContent=data.split(' ').join('_');
+    KebabCase.textContent=data.split(' ').join('-');
 }
 
 mainInput.addEventListener('input', () => textTransform({}));
-
-
-
+textTransform({});
