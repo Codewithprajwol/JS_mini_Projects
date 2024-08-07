7ec8ea76576b63a2972b1a5cf0e9d2f2f30aaa35
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2024-08-07 16:36:21 +0545

    completed toast popup

--- Toast Popup/script.js
+++ Toast Popup/script.js
@@ -8,6 +8,17 @@ const popupContainer=document.querySelector('.Popup');
 const ProgressBar=document.querySelector('#progressBar');
 
 function SelectInfo({ver,hor,mes}){
+  const popup=document.createElement('div');
+  popup.classList.add('inner-popup');
+  popup.textContent=input.value;
+  popup.classList.add(mes)
+  const closeIcon=document.createElement('span');
+  closeIcon.classList.add('close-icon')
+  closeIcon.innerText=' ✕';
+  popup.appendChild(closeIcon);
+  popupContainer.appendChild(popup)
+
+  // addding classes
   if(hor=='right'){
     popupContainer.classList.add('right');
   }
@@ -17,21 +28,13 @@ function SelectInfo({ver,hor,mes}){
 
   if( ver=='bottom'){
     popupContainer.classList.add('bottom');
+    
   }
   else{
     popupContainer.classList.remove('bottom')
   }
-  const popup=document.createElement('div');
-  popup.classList.add('inner-popup');
-  popup.textContent=input.value;
-  popup.classList.add(mes)
-  const closeIcon=document.createElement('span');
-  closeIcon.classList.add('close-icon')
-   closeIcon.innerText=' ✕';
-   popup.appendChild(closeIcon);
-  popupContainer.appendChild(popup)
 
-  popupContainer.appendChild(popup);
+// function for remove animation
   function removeAnimation(){
     if(hor == 'right'){
       popup.classList.add('hide1');
@@ -40,11 +43,15 @@ function SelectInfo({ver,hor,mes}){
       popup.classList.add('hide');
     }
   }
+
+//click remove event listner
   closeIcon.addEventListener('click',(e)=>{
     removeAnimation();
     setTimeout(()=>popup.remove(),100)
   })
-// popup.remove();
+
+
+  //adding settimeout for removing popup
   setTimeout(removeAnimation,ProgressBar.value*1000)
   setTimeout(()=>{
     popup.remove();
@@ -52,6 +59,9 @@ function SelectInfo({ver,hor,mes}){
   },ProgressBar.value+.1*1000)
   }
 
+
+  
+  // adding event listner when button is clicked
 btn.addEventListener('click',(e)=>{
    SelectInfo({ver:vertical.value ,
         hor:horizontal.value,
