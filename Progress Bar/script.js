5146a8529359f18844bbb770b43cee5247a916c1
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2024-08-11 15:28:27 +0545

    added some color and setinterval work upto 101 only

--- Progress Bar/script.js
+++ Progress Bar/script.js
@@ -3,20 +3,43 @@ const pauseBtn=document.querySelector('#pause');
 const resetBtn=document.querySelector('#reset');
 const slider=document.querySelector('.inner-progress-bar');
 
+const colorchanger=[
+    {color:'red'},
+    {color:'orange'},
+    {color:'green'},
+]
+
 let widthValue=0;
 let intervalData;
+
+
+
 startBtn.addEventListener('click',(e)=>{
     intervalData=setInterval(() => {
         slider.style.width=widthValue++ + "%";
+        
+        if(widthValue<30){
+    slider.style.backgroundColor=colorchanger[0].color;
+         }
+
+        if(widthValue>30){
+    slider.style.backgroundColor=colorchanger[1].color;
+         }
+
+        if(widthValue>70){
+    slider.style.backgroundColor=colorchanger[2].color;
+         }
+
+        if(widthValue>100){
+            pauseBtn.disabled=true;
+    pauseBtn.style.cursor="not-allowed";
+            clearInterval(intervalData)
+        }
     },100);
     startBtn.disabled=true;
     startBtn.style.cursor="not-allowed";
     pauseBtn.style.cursor="pointer";
     pauseBtn.disabled=false;
-    
-    if(widthValue>100){
-       clearInterval(intervalData); 
-    }
 })
 
 pauseBtn.addEventListener('click',(e)=>{
@@ -28,6 +51,12 @@ pauseBtn.addEventListener('click',(e)=>{
 })
 
 resetBtn.addEventListener('click',(e)=>{
+    clearInterval(intervalData)
     widthValue=0;
     slider.style.width=widthValue + "%";
+    startBtn.disabled=false;
+    startBtn.style.cursor="pointer"
+    pauseBtn.disabled=false;
+    pauseBtn.style.cursor="pointer"
+    
 })
